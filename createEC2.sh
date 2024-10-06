bf73f3180a5d1e0797a2ca52d1198d928dd4d1a4
SurisettyGit <surisetty08@gmail.com>
2024-10-06 19:43:57 +0530

    eshop-shell

---
+++ createEC2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
+
+for instance in ${instances[@]}; do
+    echo "Creating instance for : $instance"
+done
