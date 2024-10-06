225baeecd36904028c3a43e63e4f5f70ad9dd5eb
SurisettyGit <surisetty08@gmail.com>
2024-10-06 20:02:04 +0530

    eshop-shell

--- createEC2.sh
+++ createEC2.sh
@@ -3,5 +3,11 @@
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web");
 
 for instance in ${instances[@]}; do
+    if [ $instance == "shipping" ] || [ $instance == "mysql" ]
+    then
+        instance_type="t3.medium"
+    else
+        instance_type="t3.micro"
+    fi
     echo "Creating instance for : $instance"
 done
